f5f27fb17abaced4a9659c5c0e89b292933da572
yindaheng98 <yindaheng98@163.com>
2020-03-06 11:42:16 +0800

    name fix

--- example/grpc/client/Client.go
+++ example/grpc/client/Client.go
@@ -2,7 +2,7 @@ package client
 
 import (
 	"github.com/yindaheng98/gogisnet/client"
-	grpcClient "github.com/yindaheng98/gogisnet/example/grpc/protocol/client"
+	grpcServiceClient "github.com/yindaheng98/gogisnet/example/grpc/protocol/client"
 	pb "github.com/yindaheng98/gogisnet/example/grpc/protocol/protobuf"
 )
 
@@ -12,7 +12,7 @@ type Client struct {
 
 func New(ClientInfo *pb.ClientInfo, option Option) *Client {
 	ServiceOption, GRPCOption := option.ServiceOption, option.GRPCOption
-	S2CClient := grpcClient.NewS2CClient(GRPCOption)
+	S2CClient := grpcServiceClient.NewS2CClient(GRPCOption)
 	ServiceOption.RequestProto = S2CClient.NewRequestProtocol()
 	return &Client{client.New(ClientInfo, ServiceOption)}
 }
