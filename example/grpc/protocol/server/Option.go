0264079a506f9ef015316c12c3b660e7943ce007
yindaheng98 <yindaheng98@163.com>
2020-03-06 15:26:54 +0800

    Option fixed

--- example/grpc/protocol/server/Option.go
+++ example/grpc/protocol/server/Option.go
@@ -2,24 +2,16 @@ package server
 
 import (
 	"google.golang.org/grpc"
-	"net"
 )
 
 type GRPCServerOption struct {
-	Listener   net.Listener
 	InitOption []grpc.ServerOption
 	BufferLen  uint64
 }
 
-func DefaultOption(ListenNetwork, ListenAddr string) (option GRPCServerOption, err error) {
-	option = GRPCServerOption{
-		Listener:   nil,
+func DefaultOption() GRPCServerOption {
+	return GRPCServerOption{
 		InitOption: nil,
 		BufferLen:  100,
 	}
-	Listener, err := net.Listen(ListenNetwork, ListenAddr)
-	if err != nil {
-		option.Listener = Listener
-	}
-	return
 }
--- example/grpc/server/Option.go
+++ example/grpc/server/Option.go
@@ -7,28 +7,6 @@ import (
 	"github.com/yindaheng98/gogisnet/server"
 )
 
-type GRPCServerNetworkOption struct { //GRPC服务器网络设置
-	ListenNetwork string //监听哪种网络
-	ListenAddr    string //监听哪个地址
-	BoardCastAddr string //向外广播哪个地址
-}
-
-func DefaultS2SGRPCServerNetworkOption() GRPCServerNetworkOption {
-	return GRPCServerNetworkOption{
-		ListenNetwork: "tcp",
-		ListenAddr:    "locolhost:4241",
-		BoardCastAddr: "locolhost:4241",
-	}
-}
-
-func DefaultS2CGRPCServerNetworkOption() GRPCServerNetworkOption {
-	return GRPCServerNetworkOption{
-		ListenNetwork: "tcp",
-		ListenAddr:    "locolhost:4240",
-		BoardCastAddr: "locolhost:4240",
-	}
-}
-
 type Option struct {
 	ServiceOption server.Option
 	GRPCOption    GRPCOption
@@ -40,18 +18,14 @@ type GRPCOption struct {
 	S2SClientOption grpcServiceClient.GRPCClientOption
 }
 
-func DefaultWithNetworkOption(S2SGRPCServerNetworkOption, S2CGRPCServerNetworkOption GRPCServerNetworkOption, initServer *pb.S2SInfo) (option Option, err error) {
-	option = Option{GRPCOption: GRPCOption{S2SClientOption: grpcServiceClient.DefaultOption()}} //初始化
-	var e error
-	option.GRPCOption.S2SServerOption, e = grpcServiceServer.DefaultOption(S2SGRPCServerNetworkOption.ListenNetwork, S2SGRPCServerNetworkOption.ListenAddr)
-	if e != nil { //S2S服务器GRPC设置
-		err = e
+func DefaultWithNetworkOption(S2SBoardCastAddr, S2CBoardCastAddr string, initServer *pb.S2SInfo) (option Option, err error) {
+	option = Option{ //初始化
+		GRPCOption: GRPCOption{
+			S2SServerOption: grpcServiceServer.DefaultOption(),
+			S2CServerOption: grpcServiceServer.DefaultOption(),
+			S2SClientOption: grpcServiceClient.DefaultOption(),
+		},
 	}
-	option.GRPCOption.S2CServerOption, e = grpcServiceServer.DefaultOption(S2CGRPCServerNetworkOption.ListenNetwork, S2CGRPCServerNetworkOption.ListenAddr)
-	if e != nil { //S2C服务器GRPC设置
-		err = e
-	}
-
 	if initServer == nil { //构造初始轮询服务器
 		initServer = &pb.S2SInfo{
 			ServerInfo: &pb.ServerInfo{
@@ -73,17 +47,12 @@ func DefaultWithNetworkOption(S2SGRPCServerNetworkOption, S2CGRPCServerNetworkOp
 			},
 		}
 	}
-	init, e := initServer.Unpack() //解包初始轮询服务器
-	if e == nil {                  //构造服务设置
-		option.ServiceOption = server.DefaultOption(*init, nil, nil, nil)
-		option.ServiceOption.S2SRegistryOption.RequestSendOption = &pb.RequestSendOption{Addr: S2SGRPCServerNetworkOption.BoardCastAddr}
-		option.ServiceOption.S2CRegistryOption.RequestSendOption = &pb.RequestSendOption{Addr: S2CGRPCServerNetworkOption.BoardCastAddr}
+	init, err := initServer.Unpack() //解包初始轮询服务器
+	if err != nil {                  //构造服务设置
 		return
 	}
-	err = e
+	option.ServiceOption = server.DefaultOption(*init, nil, nil, nil)
+	option.ServiceOption.S2SRegistryOption.RequestSendOption = &pb.RequestSendOption{Addr: S2SBoardCastAddr}
+	option.ServiceOption.S2CRegistryOption.RequestSendOption = &pb.RequestSendOption{Addr: S2CBoardCastAddr}
 	return
-}
-
-func DefaultOption(initServer *pb.S2SInfo) (option Option, err error) {
-	return DefaultWithNetworkOption(DefaultS2SGRPCServerNetworkOption(), DefaultS2CGRPCServerNetworkOption(), initServer)
 }
