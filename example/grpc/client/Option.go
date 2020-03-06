e0305ad77ed7c2d3268538ac77726573a572fdf5
yindaheng98 <yindaheng98@163.com>
2020-03-06 11:43:02 +0800

    name fix

--- example/grpc/client/Option.go
+++ example/grpc/client/Option.go
@@ -2,18 +2,18 @@ package client
 
 import (
 	"github.com/yindaheng98/gogisnet/client"
-	grpcClient "github.com/yindaheng98/gogisnet/example/grpc/protocol/client"
+	grpcServiceClient "github.com/yindaheng98/gogisnet/example/grpc/protocol/client"
 	"github.com/yindaheng98/gogisnet/protocol"
 )
 
 type Option struct {
 	ServiceOption client.Option
-	GRPCOption    grpcClient.GRPCClientOption
+	GRPCOption    grpcServiceClient.GRPCClientOption
 }
 
 func DefaultOption(initServer protocol.S2CInfo) Option {
 	return Option{
 		ServiceOption: client.DefaultOption(initServer, nil),
-		GRPCOption:    grpcClient.DefaultOption(),
+		GRPCOption:    grpcServiceClient.DefaultOption(),
 	}
 }
