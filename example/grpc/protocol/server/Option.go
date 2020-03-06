a9c7ee19b98bc9d638f468814b3c43696e634317
yindaheng98 <yindaheng98@163.com>
2020-03-06 14:16:11 +0800

    default option fix

--- example/grpc/protocol/server/Option.go
+++ example/grpc/protocol/server/Option.go
@@ -1,15 +1,25 @@
 package server
 
-import "google.golang.org/grpc"
+import (
+	"google.golang.org/grpc"
+	"net"
+)
 
 type GRPCServerOption struct {
+	Listener   net.Listener
 	InitOption []grpc.ServerOption
 	BufferLen  uint64
 }
 
-func DefaultOption() GRPCServerOption {
-	return GRPCServerOption{
+func DefaultOption(ListenNetwork, ListenAddr string) (option GRPCServerOption, err error) {
+	option = GRPCServerOption{
+		Listener:   nil,
 		InitOption: nil,
 		BufferLen:  100,
 	}
+	Listener, err := net.Listen(ListenNetwork, ListenAddr)
+	if err != nil {
+		option.Listener = Listener
+	}
+	return
 }
