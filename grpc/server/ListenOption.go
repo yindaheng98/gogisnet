d836f1f2f010f02ec899384ec0030b6243a681a1
yindaheng98 <yindaheng98@163.com>
2020-03-20 15:01:50 +0800

    Struct tag updated

--- grpc/server/ListenOption.go
+++ grpc/server/ListenOption.go
@@ -1,9 +1,12 @@
 package server
 
 type ListenerOption struct {
-	S2SListenNetwork, S2SListenAddr               string
-	S2CListenNetwork, S2CListenAddr               string
-	GraphQueryListenNetwork, GraphQueryListenAddr string
+	S2SListenNetwork        string `yaml:"S2SListenNetwork" usage:"Network type that the S2SRegistry listen on (e.g. tcp, udp)."`
+	S2SListenAddr           string `yaml:"S2SListenAddr" usage:"Network address and port that the S2SRegistry listen on."`
+	S2CListenNetwork        string `yaml:"S2CListenNetwork" usage:"Network type that the S2CRegistry listen on (e.g. tcp, udp)."`
+	S2CListenAddr           string `yaml:"S2CListenAddr" usage:"Network address and port that the S2CRegistry listen on."`
+	GraphQueryListenNetwork string `yaml:"GraphQueryListenNetwork" usage:"Network type that the GraphQueryRegistry listen on (e.g. tcp, udp)."`
+	GraphQueryListenAddr    string `yaml:"GraphQueryListenAddr" usage:"Network address and port that the GraphQuery server listen on."`
 }
 
 func DefaultListenerOption() ListenerOption {
