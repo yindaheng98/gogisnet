3b77dd2aa45c7d7aa2464cd26d334a0a57c26454
yindaheng98 <yindaheng98@163.com>
2020-03-20 17:41:50 +0800

    fixed

--- go.mod
+++ go.mod
@@ -6,8 +6,8 @@ require (
 	github.com/golang/protobuf v1.3.5
 	github.com/google/go-cmp v0.4.0 // indirect
 	github.com/yindaheng98/go-utility v0.0.0-20200319143556-a35c01a7ba8d
-	github.com/yindaheng98/gogistry v0.0.0-20200319145125-3977e030114c
-	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	github.com/yindaheng98/gogistry v0.0.0-20200320091633-8243bc1af8ab
+	golang.org/x/net v0.0.0-20200319234117-63522dbf7eec // indirect
 	golang.org/x/sys v0.0.0-20200317113312-5766fd39f98d // indirect
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20200319113533-08878b785e9c // indirect
--- grpc/protocol/registrant/S2SRegistrant.go
+++ grpc/protocol/registrant/S2SRegistrant.go
@@ -31,7 +31,7 @@ func (c *S2SRegistrant) NewRequestProtocol() S2SRequestProtocol {
 //NewCandidateList implements a CandidateList.PingerCandidateList using current S2SRegistrant and returns its pointer.
 func (c *S2SRegistrant) NewPingerCandidateList(initServer *pb.S2SInfo, option PingerCandidateListOption) *CandidateList.PingerCandidateList {
 	InitServer, _ := initServer.Unpack()
-	l := CandidateList.NewPingerCandidateList(option.Size, InitServer, s2sPINGer{clients: c}, option.MaxPingTimeout)
+	l := CandidateList.NewPingerCandidateList(option.Size, s2sPINGer{clients: c}, option.MaxPingTimeout, InitServer)
 	l.DefaultRetryN = option.DefaultRetryN
 	l.DefaultTimeout = option.DefaultTimeout
 	return l
